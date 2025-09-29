9d3c549e20be52400d5b2384f0e12899826ab147
Marre <23345144+Marre3@users.noreply.github.com>
2025-09-29 04:44:42 +0200

    DON'T MERGE: weird parallel solution
    A weird solution that calculates all forces per tick in parallel
    with a huge array, turned out to not be faster

--- solver.py
+++ solver.py
@@ -29,9 +29,48 @@ def updateParticle(i, N, masses, positions, velocities, tickNumber, deltaTime):
     positions[tickNumber, i] = positions[tickNumber - 1, i] + velocities[tickNumber, i] * deltaTime
 
 
-def doTick(N, masses, positions, velocities, tickNumber, deltaTime):
+def c1(N, masses, positions, velocities, tickNumber, deltaTime):
+    gravity = 100/N
+    differences = positions[tickNumber - 1][:, np.newaxis] - positions[tickNumber - 1]
+    distances_squared = np.einsum("...ij,...ij->...i", differences, differences)
+    distances = (np.sqrt(distances_squared) + epsilon)
+
+    denominators = 1/(distances * distances**2)
+    x = masses * denominators
+
+    acceleration = np.einsum("...i, ...ij->...j", x, differences)
+
+    velocities[tickNumber] = velocities[tickNumber - 1] + acceleration * -gravity * deltaTime
+    positions[tickNumber] = positions[tickNumber - 1] + velocities[tickNumber] * deltaTime
+
+def c2(N, masses, positions, velocities, tickNumber, deltaTime):
     for i in range(N):
         updateParticle(i, N, masses, positions, velocities, tickNumber, deltaTime)
+
+
+def doTick(N, masses, positions, velocities, tickNumber, deltaTime):
+    #c1(N, masses, positions, velocities, tickNumber, deltaTime)
+    c2(N, masses, positions, velocities, tickNumber, deltaTime)
+
+
+    # differences = np.einsum("")
+    # for i in range(N):
+    #     pass
+        #updateParticle(i, N, masses, positions, velocities, tickNumber, deltaTime)
+
+        #x = np.einsum("ij,kl ->ikjl", positions[tickNumber - 1], neg_positions)
+        # print(x.shape)
+        # differences = differences
+        # print(neg_positions)
+        # print(positions[tickNumber - 1, i])
+        # print(differences)
+        # print(differences.shape)
+        # exit(1)
+        # differences = positions[tickNumber - 1, i] + neg_positions
+
+
+        # return -gravity * totalForce
+        # acceleration = particleAcceleration(i, N, masses, positions, tickNumber)
 
 
 def solveNbody(timespan, initialState, deltaTime, *args):
