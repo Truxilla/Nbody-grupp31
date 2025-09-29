99f3e609361ec3346051224b0b6760a566f8dcf1
Marre <23345144+Marre3@users.noreply.github.com>
2025-09-29 06:37:07 +0200

    Add some comments

--- solver.py
+++ solver.py
@@ -10,13 +10,16 @@ def particleAcceleration(
 ):
     gravity = 100/N
 
+    # Calculate differences in position (r) between particle i and each particle
+    # as an array of vectors [[xDiff1, yDiff1], [xDiff2, yDiff2], ...]
     differences = -(positions[tickNumber - 1] - positions[tickNumber - 1, i])
+
+    # Calculate the distance from i to each other particle
     differences_squared = differences * differences
-
     distances_squared = differences_squared[:, 0] + differences_squared[:, 1]
-
     distances = np.sqrt(distances_squared)
 
+    # Calculate the force for each particle on i
     denominators = (distances + epsilon)**3
     x = masses/denominators
     forces = x.reshape(N, 1) * differences
@@ -24,6 +27,7 @@ def particleAcceleration(
     # 0 force for j = i
     forces[i] = forces[i] * 0
 
+    # Sum the per-particle forces to get the total force for the particle
     totalForce = np.sum(forces, axis = 0)
 
     return totalForce * -gravity
@@ -53,24 +57,33 @@ def solveNbody(timespan, initialState, deltaTime, *args):
     outputVectorArray = np.zeros((len(timeVector), len(initialState)), dtype=object)
     outputVectorArray[0, :] = initialState
 
+    # Amount of planets/stars
     N = len(initialState)
+
+    # Create a table where masses[i] is the mass of the i:th body
     masses = np.zeros(N, dtype=float)
     for i, p in zip(range(N), initialState):
         masses[i] = p.mass
 
+    # Create a table where brightnesses[i] is the brightness of the i:th body
     brightnesses = np.zeros(N, dtype=float)
     for i, p in zip(range(N), initialState):
         brightnesses[i] = p.brightness
 
+    # Create a table where positions[t, i] is the position [x, y] of the i:th body after t time steps
+    # and fill it with the initial state t = 0
     positions = np.zeros((len(timeVector), N, 2), dtype=float)
     for i, p in zip(range(N), initialState):
         positions[0, i] = p.position
 
+    # Create a table where velocities[t, i] is the velocity [vx, vy] of the i:th body after t time steps
+    # and fill it with the initial state t = 0
     velocities = np.zeros((len(timeVector), N, 2), dtype=float)
     for i, p in zip(range(N), initialState):
         velocities[0, i] = p.velocity
 
     for tickNumber in range(1, len(timeVector)):
+        # Perform the actual computations
         print(f"Tick {tickNumber}")
         doTick(N, masses, positions, velocities, tickNumber, deltaTimeVector[tickNumber])
 
